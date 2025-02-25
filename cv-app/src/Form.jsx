21c5c60481f5a76af44832467abbe08ce5201140
dawidtt <dawid.gorszka@gmail.com>
2025-02-25 18:57:00 +0100

    Create education section in form

--- cv-app/src/Form.jsx
+++ cv-app/src/Form.jsx
@@ -8,7 +8,23 @@ function Form() {
         <h2>General Info</h2>
         <TextInput id="name" label="Name" type="text" />
         <TextInput id="email" label="Email" type="email" />
-        <TextInput id="email" label="Phone number" type="tel" />
+        <TextInput id="tel" label="Phone number" type="tel" />
+      </section>
+      <section className="general-info">
+        <h2>Education</h2>
+        <TextInput id="school-name" label="School name" type="text" />
+        <TextInput id="title-of-study" label="Title of study" type="text" />
+        <TextInput
+          id="date-of-study-start"
+          label="Starting date of study
+ "
+          type="date"
+        />
+        <TextInput
+          id="date-of-study-end"
+          label="Ending date of study"
+          type="date"
+        />
       </section>
     </form>
   );
