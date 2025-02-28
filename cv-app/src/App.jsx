71e7fc964a7bf33601969a8c2474c3c8775227d6
dawidtt <dawid.gorszka@gmail.com>
2025-02-28 17:05:59 +0100

    Move data higher in modules

--- cv-app/src/App.jsx
+++ cv-app/src/App.jsx
@@ -1,8 +1,81 @@
 import "./App.css";
 import Form from "./Form";
+import { useState } from "react";
 
 function App() {
-  return <Form />;
+  const [inputs, setInputs] = useState([
+    { id: "name", value: "" },
+    { id: "phoneNumber", value: "" },
+    { id: "email", value: "" },
+    { id: "schoolName", value: "" },
+    { id: "titleOfStudy", value: "" },
+    { id: "startSchoolDate", value: "" },
+    { id: "endSchoolDate", value: "" },
+    { id: "companyName", value: "" },
+    { id: "postion", value: "" },
+    { id: "responsibilities", value: [{ id: crypto.randomUUID(), value: "" }] },
+    { id: "startJobDate", value: "" },
+    { id: "endJobDate", value: "" },
+  ]);
+
+  function onSubmit() {}
+
+  function addInput(e) {
+    e.preventDefault();
+    setInputs((prevInputs) =>
+      prevInputs.map((input) =>
+        input.id === "responsibilities"
+          ? {
+              ...input,
+              value: [...input.value, { id: crypto.randomUUID(), value: "" }],
+            }
+          : input
+      )
+    );
+  }
+
+  function removeInput(id) {
+    setInputs((prevInputs) =>
+      prevInputs.map((input) =>
+        input.id === "responsibilities"
+          ? { ...input, value: input.value.filter((res) => res.id !== id) }
+          : input
+      )
+    );
+  }
+  function changeResponsibilityInput(responsibilityId, newValue) {
+    setInputs((prevInputs) => {
+      return prevInputs.map((input) => {
+        if (input.id === "responsibilities") {
+          return {
+            ...input,
+            value: input.value.map((res) =>
+              res.id === responsibilityId ? { ...res, value: newValue } : res
+            ),
+          };
+        }
+        return input;
+      });
+    });
+  }
+
+  function onChange(id, value) {
+    setInputs((prevInputs) =>
+      prevInputs.map((input) =>
+        input.id === id ? { ...input, value: value } : input
+      )
+    );
+  }
+  return (
+    <Form
+      onSubmit={onSubmit}
+      addInput={addInput}
+      removeInput={removeInput}
+      changeResponsibilityInput={changeResponsibilityInput}
+      onChange={onChange}
+      inputs={inputs}
+    />
+  );
 }
 
 export default App;
--- cv-app/src/Form.jsx
+++ cv-app/src/Form.jsx
@@ -1,70 +1,15 @@
-import { useState } from "react";
 import Responsibilities from "./ResponsibilitiesList";
 import TextInput from "./TextInput";
 import "./styles/Form.css";
 
-function Form() {
-  const [inputs, setInputs] = useState([
-    { id: "name", value: "" },
-    { id: "phoneNumber", value: "" },
-    { id: "email", value: "" },
-    { id: "schoolName", value: "" },
-    { id: "titleOfStudy", value: "" },
-    { id: "startSchoolDate", value: "" },
-    { id: "endSchoolDate", value: "" },
-    { id: "companyName", value: "" },
-    { id: "postion", value: "" },
-    { id: "responsibilities", value: [{ id: crypto.randomUUID(), value: "" }] },
-    { id: "startJobDate", value: "" },
-    { id: "endJobDate", value: "" },
-  ]);
-
-  function addInput(e) {
-    e.preventDefault();
-    setInputs((prevInputs) =>
-      prevInputs.map((input) =>
-        input.id === "responsibilities"
-          ? {
-              ...input,
-              value: [...input.value, { id: crypto.randomUUID(), value: "" }],
-            }
-          : input
-      )
-    );
-  }
-
-  function removeInput(id) {
-    setInputs((prevInputs) =>
-      prevInputs.map((input) =>
-        input.id === "responsibilities"
-          ? { ...input, value: input.value.filter((res) => res.id !== id) }
-          : input
-      )
-    );
-  }
-  function changeResponsibilityInput(responsibilityId, newValue) {
-    setInputs((prevInputs) => {
-      return prevInputs.map((input) => {
-        if (input.id === "responsibilities") {
-          return {
-            ...input,
-            value: input.value.map((res) =>
-              res.id === responsibilityId ? { ...res, value: newValue } : res
-            ),
-          };
-        }
-        return input;
-      });
-    });
-  }
-
-  function onChange(id, value) {
-    setInputs((prevInputs) =>
-      prevInputs.map((input) =>
-        input.id === id ? { ...input, value: value } : input
-      )
-    );
-  }
+function Form({
+  onSubmit,
+  addInput,
+  removeInput,
+  changeResponsibilityInput,
+  onChange,
+  inputs,
+}) {
   return (
     <form>
       <section className="general-info">
